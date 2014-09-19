b4dfad9ec08726a49fad3f9748842eb24a2293ab
Conrad Irwin <conrad.irwin@gmail.com>
2014-09-19 14:50:02 -0700

    Fix rails c

--- lib/console.log/core_ext.rb
+++ lib/console.log/core_ext.rb
@@ -1,7 +1,13 @@
 module ConsoleLog
   module CoreExt
-    def console
-      LSpace[:console] ||= Console.new
+    # This is a method_missing because `be rails c` does
+    # wierd stuff with console methods.
+    def method_missing(name)
+      if name.to_s == "console"
+        LSpace[:console] ||= Console.new
+      else
+        super
+      end
     end
   end
 end
